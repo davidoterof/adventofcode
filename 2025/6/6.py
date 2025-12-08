595991183b6227662413497ebcf6aa4ca27f7e8c
David Otero <david.otero.freijeiro@udc.es>
2025-12-08 17:05:54 +0100

    Add day 6 part 1

---
+++ 2025/6/6.py
@@ -0,0 +1,28 @@
+import operator
+import sys
+from functools import reduce
+
+ops = {"+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.truediv}
+
+
+def solve_p1_1(ruta):
+    with open(ruta, "r") as file:
+        lines = file.readlines()
+
+    n_operations = len(lines[0].strip().split())
+    arrays = [[] for _ in range(n_operations)]
+    operations = lines[-1].strip().split()
+    data = lines[:-1]
+    for line in data:
+        numbers = line.strip().split()
+        for i, elem in enumerate(numbers):
+            arrays[i].append(float(elem))
+
+    result = sum(
+        [reduce(ops[operacion], array) for operacion, array in zip(operations, arrays)]
+    )
+
+    print(int(result))
+
+
+solve_p1_1(sys.argv[1])
