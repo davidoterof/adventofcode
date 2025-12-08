773a1c4c293a639175425bd5efbf928c58c380c3
David Otero <david.otero.freijeiro@udc.es>
2025-12-08 17:20:19 +0100

    Add day 7 part 1

---
+++ 2025/7/7.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def solve_p1_1(ruta):
+    with open(ruta, "r") as file:
+        lines = file.readlines()
+
+    rays = set()
+    rays.add(lines[0].index("S"))
+    split = 0
+    for line in lines[1:]:
+        for i in rays.copy():
+            if line[i] == "^":
+                rays.add(i - 1)
+                rays.add(i + 1)
+                rays.remove(i)
+                split += 1
+
+    print(split)
+
+
+solve_p1_1(sys.argv[1])
